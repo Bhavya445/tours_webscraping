2ee425b592013a30a858371c43bd2a0c15db7925
Bhavya445 <bhavya4405@gmail.com>
2024-06-21 02:03:55 +0530

    scraping data

--- pythonProject10/main.py
+++ pythonProject10/main.py
@@ -1,10 +1,16 @@
 import requests
 import selectorlib
-
+import smtplib, ssl
+import os
+import time
+import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+connection = sqlite3.connect("data.db")
+
 
 def scrape(url):
     """scrape the page source from url"""
@@ -13,6 +19,57 @@ def scrape(url):
     return source
 
 def extract(source):
-    selectorlib.Extractor.from_yaml_file("extract.yaml")
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def store(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "liveauthentic445@gmail.com"
+    password = "kyvh szap uoxy ccbe"
+
+    receiver = "bhavya4405@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+    print("email sent")
+
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band,city,date) )
+    rows = cursor.fetchall()
+
+    print(rows)
+    return rows
+
+
+
 if __name__ == "__main__":
-    print(scrape(URL))
+    while True:
+        extracted = extract(scrape(URL))
+        print(extracted)
+
+
+        if extracted != "No upcoming tours":
+            row = read(extracted)
+            if not row:
+                store(extracted)
+                send_email(extracted)
+        time.sleep(2)
