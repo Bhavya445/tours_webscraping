c2ff33c9573aeb9bd2eb707a9c16dc5cd433ae87
Bhavya445 <bhavya4405@gmail.com>
2024-06-21 00:00:19 +0530

    Initial commit

---
+++ pythonProject10/main.py
@@ -0,0 +1,18 @@
+import requests
+import selectorlib
+
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def scrape(url):
+    """scrape the page source from url"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+def extract(source):
+    selectorlib.Extractor.from_yaml_file("extract.yaml")
+if __name__ == "__main__":
+    print(scrape(URL))
